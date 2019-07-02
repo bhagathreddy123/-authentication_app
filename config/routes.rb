4b63476d8dcf432c538a348119d32d935d7a4123
Bhagath Reddy <katta04.bhagath2gmail.com>
2019-07-02 22:34:10 +0530

    login logout sessions registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'static#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: :create
+  resources :registrations, only: :create
+  get :loged_in, to: "sessions#logged_in"
+  get :logout, to: "sessions#logout"
+
 end
