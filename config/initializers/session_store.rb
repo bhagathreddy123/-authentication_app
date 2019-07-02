a6de922b80dea69af35459e542215dd26bda43b0
Bhagath Reddy <katta04.bhagath2gmail.com>
2019-07-02 22:41:46 +0530

    dynamic enviroment for session storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+if Rails.env  ==  "production"
+	Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
